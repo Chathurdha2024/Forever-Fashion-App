39b276b9ab5f21fbad5060cd140163b6f1e7aa03
Chathurdha2024 <hasangidahanayake22@gmail.com>
2025-08-21 18:22:00 +0530

    Created the UI of the Orders Page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -13,85 +13,83 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
-  const [cartItems,setCartItems] = useState({});
+  const [cartItems, setCartItems] = useState({});
   const navigate = useNavigate();
 
-  
-  const addToCart = async (itemId,size) => {
-
-    {/*Display an error message if we didn't select a product size as s,m,l,xl*/}    
+  const addToCart = async (itemId, size) => {
+    {
+      /*Display an error message if we didn't select a product size as s,m,l,xl*/
+    }
     if (!size) {
-      toast.error('Select Product Size');
+      toast.error("Select Product Size");
       return;
     }
 
-
-
-    
     let cartData = structuredClone(cartItems);
 
     if (cartData[itemId]) {
       if (cartData[itemId][size]) {
-        cartData[itemId][size] += 1;     {/*items with same itemId and same size(S,M,L,XL) are added together inside the cart*/}
+        cartData[itemId][size] += 1;
+        {
+          /*items with same itemId and same size(S,M,L,XL) are added together inside the cart*/
+        }
+      } else {
+        cartData[itemId][size] = 1;
+        {
+          /*items with same itemId and different sizes are added as a new entry to the cart*/
+        }
       }
-      else{
-        cartData[itemId][size] = 1;      {/*items with same itemId and different sizes are added as a new entry to the cart*/}
-      }
-    }
-    else{
+    } else {
       cartData[itemId] = {};
-      cartData[itemId][size] = 1;        {/*items with different itemId's that means different items are added as a seperate new entry to the cart*/}
+      cartData[itemId][size] = 1;
+      {
+        /*items with different itemId's that means different items are added as a seperate new entry to the cart*/
+      }
     }
 
     setCartItems(cartData);
-
-  }
+  };
 
   const getCartCount = () => {
-     let totalCount = 0;
-     for(const items in cartItems){
-        for(const item in cartItems[items]){
-            try {
-              if (cartItems[items][item] > 0) {
-                totalCount += cartItems[items][item];
-              }
-            } catch (error) {
-              
-            }
-        }
-     }
-     return totalCount;
-  }
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {}
+      }
+    }
+    return totalCount;
+  };
 
-  const updateQuantity = async (itemId,size,quantity) => {
-    
+  const updateQuantity = async (itemId, size, quantity) => {
     let cartData = structuredClone(cartItems);
 
     cartData[itemId][size] = quantity;
 
     setCartItems(cartData);
-  }
+  };
 
   const getCartAmount = () => {
-      let totalAmount = 0;
-      for(const items in cartItems){
-         let itemInfo = products.find((product)=> product._id === items );
-         for(const item in cartItems[items]){
-          try {
-            if (cartItems[items][item] > 0) {
-              totalAmount += itemInfo.price * cartItems[items][item];
-            }
-          } catch (error) {
-            
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item];
           }
-         }
+        } catch (error) {}
       }
-      return totalAmount;
+    }
+    return totalAmount;
+  };
+
+  {
+    /*when we add variable,state variable or a function inside this value object then we can access it in any component using the context API */
   }
-
-
-  
-  {/*when we add variable,state variable or a function inside this value object then we can access it in any component using the context API */}
   const value = {
     products,
     currency,
@@ -105,7 +103,7 @@ const ShopContextProvider = (props) => {
     getCartCount,
     updateQuantity,
     getCartAmount,
-    navigate 
+    navigate,
   };
 
   return (
